5d04589b647a0802c6fc5337f3e1f20515f6076d
Carlyn Patterson Gentile <pattersonc@email.chop.edu>
2019-02-24 08:02:12 -0500

    added code to deal with nulling adjustment values

---
+++ code/getMeanAdjust.m
@@ -0,0 +1,24 @@
+function [LMadjust Sadjust]=getMeanAdjust()
+
+red_above=input('red above=');
+red_below=input('red below=');
+
+green_above=input('green above=');
+green_below=input('green below=');
+
+blue_above=input('blue above=');
+blue_below=input('blue below=');
+
+yellow_above=input('yellow above=');
+yellow_below=input('yellow below=');
+
+red=-1*mean([red_above red_below]);
+green=mean([red_above red_below]);
+
+blue=-1*mean([blue_above blue_below]);
+yellow=mean([yellow_above yellow_below]);
+
+LMadjust=mean([red green]);
+Sadjust=mean([blue yellow]);
+
+end
