1d778be8b4140447396fe059cab8a7222c8d63b9
Carlyn Patterson Gentile <pattersonc@email.chop.edu>
2019-02-27 14:28:35 -0500

    updates and bug fixes

--- code/getMeanAdjust.m
+++ code/getMeanAdjust.m
@@ -1,4 +1,4 @@
-function [LMadjust Sadjust]=getMeanAdjust()
+function [LM1 S1 LM2 S2]=getMeanAdjust()
 
 red_above=input('red above=');
 red_below=input('red below=');
@@ -12,13 +12,22 @@ blue_below=input('blue below=');
 yellow_above=input('yellow above=');
 yellow_below=input('yellow below=');
 
-red=-1*mean([red_above red_below]);
+red=mean([red_above red_below]);
 green=mean([green_above green_below]);
 
-blue=-1*mean([blue_above blue_below]);
+blue=mean([blue_above blue_below]);
 yellow=mean([yellow_above yellow_below]);
 
-LMadjust=mean([red green]);
-Sadjust=mean([blue yellow]);
+LM1=mean([red green]);
+S1=mean([blue yellow]);
+
+red=min([red_above red_below]);
+green=max([green_above green_below]);
+
+blue=min([blue_above blue_below]);
+yellow=max([yellow_above yellow_below]);
+
+LM2=mean([red green]);
+S2=mean([blue yellow]);
 
 end
